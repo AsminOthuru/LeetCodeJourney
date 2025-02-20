b4845f76c71beaa6348065587ce425376d70fc30
Asmin Othuru <118998285+AsminOthuru@users.noreply.github.com>
2025-02-20 14:49:05 +0530

    Time: 2 ms (7.14%) | Memory: 41.3 MB (51.55%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean check(int[] nums) {
+        if(nums.length==0 || nums.length==1){
+            return true;
+        }
+     int i=1;
+        
+        while(i<=nums.length-1 && nums[i-1] <= nums[i]){
+
+            i++;
+
+        }
+        int val=i;
+
+        int start=i+1;
+        for(int m=start;m<nums.length;m++){
+            if(!(nums[m]>=nums[m-1])){
+
+                return false;
+            }
+
+
+        }
+
+
+
+
+        Stack<Integer> stack=new Stack<>();
+        for(int j=val;j<nums.length;j++){
+
+            int currVal =nums[j];
+ 
+            for(int k=0;k<val;k++){
+                stack.push(nums[k]);
+
+            }
+
+            for(int k=0;k<val;k++){
+                if(currVal <= nums[k]){
+                    stack.pop();
+                }
+
+            }
+            if(!stack.isEmpty()){
+                return false;
+            }
+
+
+
+
+        }
+     
+        return true;
+        
+    }
+}
